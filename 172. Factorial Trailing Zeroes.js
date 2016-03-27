8ee96093a25228407687cb80484fde7b18cd7620
Lijuan Dai <tinadlj@users.noreply.github.com>
2016-03-26 22:15:10 -0700

    Update 172. Factorial Trailing Zeroes.js

--- 172. Factorial Trailing Zeroes.js
+++ 172. Factorial Trailing Zeroes.js
@@ -3,13 +3,11 @@
  * @return {number}
  */
 var trailingZeroes = function(n) {
-    if(n===0)
-        return 0;
     var n5=0;
-    t=Math.floor(Math.log(n)/Math.log(5));
-    for(var i=1;i<=t;i++)
-    {
-        n5+=Math.floor(n/Math.pow(5,i));
+    var x=5;
+    while (n>=x){
+        n5+=Math.floor(n/x);
+        x*=5;
     }
     
     return n5;
