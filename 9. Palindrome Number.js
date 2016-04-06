762ce3117b56eea6c3937e7457ad427904b4757d
Lijuan Dai <tinadlj@users.noreply.github.com>
2016-04-05 23:11:43 -0700

    Create 9. Palindrome Number.js

---
+++ 9. Palindrome Number.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    if (x<0)
+        return false;
+    
+    if (x.toString()==x.toString().split("").reverse().join(""))
+        return true;
+    
+    return false;
+};
