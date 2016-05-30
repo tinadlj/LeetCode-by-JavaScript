b03e8a968e14cd3b63d6c24c08ca9dcd7638fbd0
Lijuan Dai <tinadlj@users.noreply.github.com>
2016-05-30 16:20:25 -0700

    Create 165. Compare Version Numbers.js

---
+++ 165. Compare Version Numbers.js
@@ -0,0 +1,33 @@
+/**
+ * @param {string} version1
+ * @param {string} version2
+ * @return {number}
+ */
+var compareVersion = function(version1, version2) {
+    var arr1=version1.split('.');
+    var arr2=version2.split('.');
+    
+    var len=Math.max(arr1.length,arr2.length);
+    var index=0;
+    
+    while(index<len){
+        var v1=parseInt(arr1[index]);
+        var v2=parseInt(arr2[index]);
+        
+        if (isNaN(v1)&&v2!==0){
+            return -1;
+        }
+        
+        if (isNaN(v2)&&v1!==0){
+            return 1;;
+        }
+        
+        if (v1>v2){
+            return 1;
+        }else if(v1<v2){
+            return -1;
+        }
+        index++;
+    }
+    return 0;
+};
