2e0331218138180c66c4c0624cfdc4a0c33c2d38
Lijuan Dai <tinadlj@users.noreply.github.com>
2016-06-01 20:59:48 -0700

    Create 13. Roman to Integer.js

---
+++ 13. Roman to Integer.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(s) {
+    var sum=0;
+    
+    var dic={
+        'I':  1,
+        'V':  5,
+        'X':  10,
+        'L':  50,
+        'C':  100,
+        'D':  500,
+        'M':  1000
+    }
+    
+    for(var i=0;i<s.length;i++){
+        if (i===s.length-1 || dic[s[i]]>=dic[s[i+1]]){
+            sum+=dic[s[i]];
+        }else{
+            sum-=dic[s[i]];
+        }
+    }
+  
+    return sum;  
+};
