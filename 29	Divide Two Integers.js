cb22d10cb0c915e040cc1878dcc5bb986af1df8d
Lijuan Dai <tinadlj@users.noreply.github.com>
2016-06-06 16:56:54 -0700

    Create 29	Divide Two Integers.js

---
+++ 29	Divide Two Integers.js
@@ -0,0 +1,6 @@
+/*Hints:
+1. No multiplication, division and mod operator: use bit manipulation << 1(*2) and >>1(/2)
+2. Multiply divisor by 2 until dividend is smaller than divisor.
+任何一个整数可以表示成以2的幂为底的一组基的线性组合，即num=a_0*2^0+a_1*2^1+a_2*2^2+…+a_n*2^n
+a_n is eithor 0 or 1
+*/
