932d16f768a66a709ce7fdd174c24c44615833ba
Lijuan Dai <tinadlj@users.noreply.github.com>
2016-06-11 15:36:44 -0700

    Create 100. Same Tree.js

---
+++ 100. Same Tree.js
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+ //similar to symmertric tree question
+var isSameTree = function(p, q) {
+    if (p===null && q===null)
+        return true;
+    
+    if (p===null || q===null || p.val!=q.val)
+        return false;
+        
+    return isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
+    
+};
