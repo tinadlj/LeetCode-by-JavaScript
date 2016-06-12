25423a03610d2bf2a64c6e5befaedb64cfa15cc6
Lijuan Dai <tinadlj@users.noreply.github.com>
2016-06-11 22:05:39 -0700

    Create 332. Reconstruct Itinerary.js

---
+++ 332. Reconstruct Itinerary.js
@@ -0,0 +1,40 @@
+/**
+ * @param {string[][]} tickets
+ * @return {string[]}
+ */
+var findItinerary = function(tickets) {
+    var aArr={};
+    for (var i=0;i<tickets.length;i++){
+        var t;
+        if(!aArr[tickets[i][0]]){
+            t=[];
+        }
+        else{
+            t=aArr[tickets[i][0]];
+        }
+        t.push(tickets[i][1]);
+        aArr[tickets[i][0]]=t;
+    }
+    
+    return solve('JFK',aArr);
+    
+};
+
+function solve(start,ticketsArr){
+    var left=[];
+    var right=[];
+    if (ticketsArr[start]){
+       ticketsArr[start].sort();
+       while(ticketsArr[start].length>0){
+        var end=ticketsArr[start].shift();
+        var subroutes=solve(end, ticketsArr);
+        if (subroutes.indexOf(start)!==-1){
+            left=left.concat(subroutes);
+        }
+        else{
+            right=right.concat(subroutes);
+        }
+    }
+    }
+     return [start].concat(left,right);
+}
