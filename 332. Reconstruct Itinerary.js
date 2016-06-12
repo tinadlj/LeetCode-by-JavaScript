6fc159e844f825ea951e9538de9f8ead825da0d1
Lijuan Dai <tinadlj@users.noreply.github.com>
2016-06-11 23:13:11 -0700

    Update 332. Reconstruct Itinerary.js

--- 332. Reconstruct Itinerary.js
+++ 332. Reconstruct Itinerary.js
@@ -38,3 +38,31 @@ function solve(start,ticketsArr){
     }
      return [start].concat(left,right);
 }
+
+// use Euler Graph
+var findItinerary = function(tickets) {
+    var aArr={};
+    for (var i=0;i<tickets.length;i++){
+        var t;
+        if(!aArr[tickets[i][0]]){
+            t=[];
+        }
+        else{
+            t=aArr[tickets[i][0]];
+        }
+        t.push(tickets[i][1]);
+        t.sort();
+        aArr[tickets[i][0]]=t;
+        
+    }
+    path=[]
+    visit('JFK',aArr,path);
+    return path;
+};
+
+function visit(start,ticketsArr,path){
+    while(ticketsArr[start] && ticketsArr[start].length>0){
+        visit(ticketsArr[start].shift(),ticketsArr,path);
+    }
+    path.unshift(start);
+}
