78b07a75c0f9fb0b6a2fb88f86f9d61e9c4c86e6
Lijuan Dai <tinadlj@users.noreply.github.com>
2016-06-15 17:17:27 -0700

    Update 206. Reverse Linked List.js

--- 206. Reverse Linked List.js
+++ 206. Reverse Linked List.js
@@ -10,26 +10,19 @@
  * @return {ListNode}
  */
 var reverseList = function(head) {
-    
-    if (!head ||!head.next)
+    if (!head || !head.next){
         return head;
+    }
+    var cur=head.next,pre=head;
+    
+    while(cur){
+        var t=cur.next;
         
-    var t=head; // get temp node which needs to be pointed to
-    var a=head.next; // a is current node
-    var b=a.next; // b is next node which lose connection with the previous nodes
+        cur.next=pre;
+        pre=cur;
+        cur=t;
+    }
     head.next=null;
     
-    do {
-        a.next=t; // first step, change current node to point to previous node
-        t=a; //second, move previous node one forward
-        a=b; //third, move current node one forward
-        if (b!==null)
-        {
-            //a=b;
-            b=b.next; //last, if there is more node, keep the futurn node connection.
-        }
-    }while(a!==null); // if current node is null, stop
-    return t; // finally t is the last iteration's current node
-    
-    
+    return pre;
 };
