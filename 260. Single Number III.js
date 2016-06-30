dfea637351c288cd34758286a92277dc08b10b19
Lijuan Dai <tinadlj@users.noreply.github.com>
2016-06-29 22:00:13 -0700

    Update 260. Single Number III.js

--- 260. Single Number III.js
+++ 260. Single Number III.js
@@ -2,6 +2,28 @@
  * @param {number[]} nums
  * @return {number[]}
  */
+ //bit manipulation
+ /**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var singleNumber = function(nums) {
+    
+    var a=0,b=0;
+    var xor=nums.reduce((a,b)=>a^b);
+    var lowbit=xor&(-xor);
+    for (var i=0;i<nums.length;i++){
+        if (nums[i]&lowbit){
+            a^=nums[i];
+        }else {
+            b^=nums[i];
+        }
+    }
+    
+    
+    return [a,b];
+};
+ //hash table/stack
 var singleNumber = function(nums) {
     var r=[];
     for(var i=0;i<nums.length;i++){
